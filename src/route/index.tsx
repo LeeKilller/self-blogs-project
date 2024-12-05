75a42e34ae75b1a06909c13df67d437afb68bbf9
Qile Lee <1750105862@qq.com>
2024-12-05 14:04:44 +0800

    add 路由骨架搭建

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,23 +1,29 @@
-
-import { createBrowserRouter } from "react-router";
-import { Navigate } from "react-router";
-import PageException from "@/views/PageException";
+import { createBrowserRouter, Navigate } from "react-router";
 import type { RouteObject } from "react-router";
+import {
+    PageException,
+    Login
+} from "@/views";
 
-const rootRoute: RouteObject [] = [
+const rootRoute: RouteObject[] = [
     {
-        index:true,
-        element:<Navigate to='/404' />
-    },
-    {
-        path:'*',
+        index: true,
         element: <Navigate to='/404' />
     },
     {
-        path:'/404',
-        element:<PageException />,
-        loader:() => ({status:'404'})
+        path: '/login',
+        element: <Login />
     },
+    {
+        path: '*',
+        element: <Navigate to='/404' />
+    },
+    {
+        path: '/404',
+        element: <PageException />,
+        loader: () => ({ status: '404' })
+    },
+
 ]
 
 
