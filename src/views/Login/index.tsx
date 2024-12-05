bd8c2b74cf2377b08a3358489042e689aa02bbab
Qile Lee <1750105862@qq.com>
2024-12-05 14:05:25 +0800

    add 登录页面编写

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,11 +1,45 @@
 import { FC } from 'react';
+import { Button, Form, Input } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import styles from './login.module.less';
+import logo from '@/assets/header.jpg'
 
 
-const Login:FC = () => {
+const Login: FC = () => {
+
+    const { Item } = Form;
+    const {
+        loginWrapper,
+        loginBox,
+        loginHeader,
+        loginForm,
+        loginButton
+    } = styles
 
     return (
-        <div>
-            Login Page.
+        <div className={loginWrapper}>
+            <div className={loginBox}>
+                <header className={loginHeader}>
+                    <div>
+                        <img src={logo} />
+                        <h2>Si Blogs</h2>
+                    </div>
+                    <h3>Si Blogs 管理后台登入</h3>
+                </header>
+                <Form className={loginForm}>
+                    <Item>
+                        <Input prefix={<UserOutlined />} placeholder='用户名' size='large' />
+                    </Item>
+                    <Item>
+                        <Input.Password prefix={<LockOutlined />} placeholder='密码'  size='large' />
+                    </Item>
+                    <Item>
+                        <Button htmlType='button' type='primary' className={loginButton}  size='large' >
+                            登录
+                        </Button>
+                    </Item>
+                </Form>
+            </div>
         </div>
     )
 }
