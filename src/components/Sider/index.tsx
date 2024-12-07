84f228176e5d6735a313e277eae9c8d3a7c0053b
Qile Lee <1750105862@qq.com>
2024-12-07 21:15:46 +0800

    feat 更新admin界面的sider

--- src/components/Sider/index.tsx
+++ src/components/Sider/index.tsx
@@ -1,19 +1,37 @@
 import { FC } from 'react';
 import { Menu } from 'antd';
 import { Link } from 'react-router';
+import { 
+    LineChartOutlined,
+    FileTextOutlined,
+    ContainerOutlined,
+    PictureOutlined
+} from '@ant-design/icons';
+import styles from './sider.module.less';
 
 
 
 const Sider: FC = () => {
     const { Item } = Menu;
+    const {
+        siderWrapper
+    } = styles;
 
     return (
-        <aside>
+        <aside className={siderWrapper}>
             <Menu>
-                <Item></Item>
-                <Item></Item>
-                <Item></Item>
-                <Item></Item>
+                <Item>
+                    <Link to='/admin/analyse'><LineChartOutlined />分析概览</Link>
+                </Item>
+                <Item>
+                    <Link to='/admin/articles'><FileTextOutlined />文章管理</Link>
+                </Item>
+                <Item>
+                    <Link to='/admin/darfts'><ContainerOutlined />草稿管理</Link>
+                </Item>
+                <Item>
+                    <Link to='/admin/picture'><PictureOutlined />图片管理</Link>
+                </Item>
             </Menu>
         </aside>
     )
--- src/layout/AdminLayout/index.tsx
+++ src/layout/AdminLayout/index.tsx
@@ -1,14 +1,21 @@
 import { FC } from 'react';
 import { Outlet } from 'react-router';
 import Sider from '@/components/Sider';
+import styles from './adminlayout.module.less';
+
+
+const {
+    adminLayoutWrapper
+} = styles;
 
 const AdminLayout: FC = () => {
+    
 
     return (
-        <>
+        <div className={adminLayoutWrapper}>
             <Sider />
             <Outlet />
-        </>
+        </div>
     )
 }
 
